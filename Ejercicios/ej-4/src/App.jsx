f43f64fdfb250dd2a5b8f6e0077bb0b1de1e54a8
Jean <jeansueiro60@gmail.com>
2024-05-01 19:09:57 -0300

    applied styles

--- Ejercicios/ej-4/src/App.jsx
+++ Ejercicios/ej-4/src/App.jsx
@@ -1,29 +1,36 @@
-// App.jsx
+import "./App.css";
 import React from "react";
 import Movie from "./components/Movie";
 
 function App() {
   return (
-    <div>
+    <div className="movieCardContainer">
       <h1>Listado de Películas</h1>
-      <div className="movie-list">
-        <Movie
-          name="Spider-man"
-          year={2017}
-          image="https://dam.smashmexico.com.mx/wp-content/uploads/2018/03/femme-fatale-spider-man-homecoming-2-jessica-drew.jpg"
-          score="bueno"
-        />
-        <Movie
-          name="Batman"
-          year={2005}
-          image="https://i.pinimg.com/originals/92/b4/a6/92b4a64312055fc4ade1e6254835d6bf.png"
-          score="regular"
-        />
-        <Movie
-          name="Batman & Robin"
-          year={1997}
-          image="https://upload.wikimedia.org/wikipedia/en/3/37/Batman_%26_Robin_poster.jpg"
-        />
+      <div className="movieCardWrapper">
+        <div className="card">
+          <Movie
+            name="Spider-man"
+            year={2017}
+            image="https://dam.smashmexico.com.mx/wp-content/uploads/2018/03/femme-fatale-spider-man-homecoming-2-jessica-drew.jpg"
+            score="bueno"
+          />
+        </div>
+        <div className="card">
+          <Movie
+            name="Batman"
+            year={2005}
+            image="https://i.pinimg.com/originals/92/b4/a6/92b4a64312055fc4ade1e6254835d6bf.png"
+            score="regular"
+          />
+        </div>
+        <div className="card">
+          <Movie
+            name="Batman & Robin"
+            year={1997}
+            image="https://upload.wikimedia.org/wikipedia/en/3/37/Batman_%26_Robin_poster.jpg"
+            score="no la he visto"
+          />
+        </div>
       </div>
     </div>
   );
--- Ejercicios/ej-4/src/components/Movie.jsx
+++ Ejercicios/ej-4/src/components/Movie.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 const Movie = ({ name, year, image, score }) => {
   return (
-    <div className="movie">
+    <div>
       <h2>{name} ({year})</h2> 
-      <img src={image} alt={name} className='movie-image'/>
       <p>Puntaje: {score}</p>
+      <img src={image} alt={name}/>
     </div>
   );
 };
