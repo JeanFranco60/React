b94d039cf53bd85edf19b2deb8d08f1fd922a1ce
Jean <jeansueiro60@gmail.com>
2024-05-04 21:41:30 -0300

    delet my project

--- my-project/my-project/src/components/Description.jsx
+++
@@ -1,9 +0,0 @@
-//Description, el cual debe renderizar un elemento <p> que muestre “Mi nombre es ” + nombre, siendo nombre una prop.
-
-import React from 'react';
-
-function Description({ nombre }) {
-  return <p>Mi nombre es {nombre}</p>;
-}
-
-export default Description;
--- my-project/my-project/src/components/Subtitle.jsx
+++
@@ -1,9 +0,0 @@
-// Subtitle, el cual debe renderizar un elemento h2 con un subtítulo (pasado por prop)
-
-import React from 'react';
-
-function Subtitle({ subtitle }) {
-  return <h2>{subtitle}</h2>;
-}
-
-export default Subtitle;
